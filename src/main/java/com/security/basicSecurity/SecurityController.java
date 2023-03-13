b03e772b44f4330e5b789f88d9033cb1fabf4c99
ANHYUCK8150 <blackhyuck@gmail.com>
2023-03-13 23:56:48 +0900

    Feat: Filter 추가

--- src/main/java/com/security/basicSecurity/SecurityController.java
+++ src/main/java/com/security/basicSecurity/SecurityController.java
@@ -10,9 +10,26 @@ public class SecurityController {
     public String index(){
         return "home";
     }
+    @GetMapping("/user")
+    public String user(){
+        return "user";
+    }
+    @GetMapping("/admin/pay")
+    public String adminPay(){
+        return "adminPay";
+    }
+    @GetMapping("/admin/**")
+    public String admin(){
+        return "admin";
+    }
 
-    @GetMapping("/loginPage")
-    public String loginPage(){
-        return "loginPage";
+    @GetMapping("/denied")
+    public String denied(){
+        return "Access is denied";
+    }
+
+    @GetMapping("/login")
+    public String login(){
+        return "login";
     }
 }

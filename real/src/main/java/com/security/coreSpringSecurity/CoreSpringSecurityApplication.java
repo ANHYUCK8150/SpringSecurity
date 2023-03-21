bf2187545ebe5ac3a683b702ae2d938de709d560
ANHYUCK8150 <blackhyuck@gmail.com>
2023-03-22 01:49:21 +0900

    Home Setting

--- real/src/main/java/com/security/coreSpringSecurity/CoreSpringSecurityApplication.java
+++ real/src/main/java/com/security/coreSpringSecurity/CoreSpringSecurityApplication.java
@@ -2,6 +2,10 @@ package com.security.coreSpringSecurity;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 public class CoreSpringSecurityApplication {
